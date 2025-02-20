6fb9dd5111d93274854cb8dcac074ae11e3a7bfb
Hoon Cho <david.hoontime@gmail.com>
2025-02-20 23:33:03 +0100

    initial commit

---
+++ apps/jjmusic-api/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'jjmusic-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/jjmusic-api',
+};
