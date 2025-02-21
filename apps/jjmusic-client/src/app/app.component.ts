89bf67f9a9f1cd15bfb70176545b8b2bcdcc8fc7
Hoon Cho <david.hoontime@gmail.com>
2025-02-22 00:07:54 +0100

    chore: add primeng

--- apps/jjmusic-client/src/app/app.component.ts
+++ apps/jjmusic-client/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { CommonModule } from '@angular/common';
+import { PrimeNgComponent } from './primeng.component';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  standalone: true,
+  imports: [CommonModule, RouterModule, PrimeNgComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
