ea9abeed7f2c23db900eb252fdee19d43d958311
Hoon Cho <david.hoontime@gmail.com>
2025-02-23 22:12:24 +0100

    chore: setup tailwindcss in angular project

--- apps/jjmusic-client/src/app/app.component.ts
+++ apps/jjmusic-client/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { PrimeNgComponent } from './primeng.component';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, RouterModule, PrimeNgComponent],
+  imports: [CommonModule, RouterModule, ButtonModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
