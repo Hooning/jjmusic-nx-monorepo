e2e382deafaf5869d2b4ac495d925952eae6011d
Hoon Cho <david.hoontime@gmail.com>
2025-02-25 23:20:34 +0100

    chore: configure ng-openapi-gen

--- apps/jjmusic-api/src/main.ts
+++ apps/jjmusic-api/src/main.ts
@@ -1,20 +1,37 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('JJMusic API')
+    .setDescription('The JJMusic API description')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Save the OpenAPI spec to a file
+  const outputPath = path.resolve(process.cwd(), 'openapi.json');
+  fs.writeFileSync(outputPath, JSON.stringify(document, null, 2), {
+    encoding: 'utf8',
+  });
+
+  // Serve Swagger UI
+  SwaggerModule.setup('api', app, document);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
   );
 }
 
