5464d515118547d4746748373ca2b3757015dd71
Hoon Cho <david.hoontime@gmail.com>
2025-03-26 22:54:17 +0100

    chore: update register request and response dtos

--- libs/api/domain/auth/src/lib/dto/auth-response.dto.ts
+++ libs/api/domain/auth/src/lib/dto/auth-response.dto.ts
@@ -1,11 +1,36 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
 export class AuthResponseDto {
+  @ApiProperty({
+    example: true,
+    description: 'Indicates whether the request was successful',
+  })
   success!: boolean;
+
+  @ApiProperty({
+    example: 'User registered successfully',
+    description: 'The message returned to the user',
+  })
   message!: string;
+
+  @ApiProperty({
+    example: 'user123',
+    description: 'The ID of the user',
+  })
   userId?: string;
 
+  @ApiPropertyOptional({
+    example:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyMTIzIiwiaWF0IjoxNjI5MDQ3Njg0LCJleHAiOjE2MjkwNDc2ODV9.1JXg1yRv7L8ZK8f3q8Z1s9K3zv0z3J1v9jz9J1z3J1',
+    description: 'The JWT token for the user',
+  })
   // Optional: Include token if you authenticate users immediately after registration
   token?: string;
 
+  @ApiPropertyOptional({
+    example: 3600,
+    description: 'The time in seconds until the token expires',
+  })
   // Optional: Include additional fields like expiresIn if using JWT
   expiresIn?: number;
 }
--- libs/api/domain/auth/src/lib/dto/register.dto.ts
+++ libs/api/domain/auth/src/lib/dto/register.dto.ts
@@ -1,23 +1,48 @@
 import { RegisterDto as IRegisterDto } from '@jjmusic-nx-monorepo/shared/data-access';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, IsBoolean, MinLength } from 'class-validator';
 
 export class RegisterDto implements IRegisterDto {
+  @ApiProperty({
+    example: 'user@example.com',
+    description: 'The email of the user',
+  })
   @IsEmail()
   email!: string;
 
+  @ApiProperty({
+    example: 'password123',
+    description: 'The password of the user',
+  })
   @IsString()
   @MinLength(8)
   password!: string;
 
+  @ApiProperty({
+    example: 'user123',
+    description: 'The username of the user',
+  })
   @IsString()
   username?: string;
 
+  @ApiProperty({
+    example: true,
+    description: 'Whether the user has accepted the terms of service',
+  })
   @IsBoolean()
   termsAccepted!: boolean;
 
+  @ApiProperty({
+    example: true,
+    description: 'Whether the user has accepted the privacy policy',
+  })
   @IsBoolean()
   privacyPolicyAccepted!: boolean;
 
+  @ApiProperty({
+    example: false,
+    description: 'Whether the user has accepted analytics tracking',
+  })
   @IsBoolean()
   analyticsConsent!: boolean;
 }
